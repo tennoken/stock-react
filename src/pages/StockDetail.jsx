2ca8d0f62c07308c373b6e26dab4f4e05ea3a592
Kzero <tennoken@naver.com>
2022-12-16 14:55:05 +0900

    add dayjs & update oneDay chart

--- src/pages/StockDetail.jsx
+++ src/pages/StockDetail.jsx
@@ -3,47 +3,51 @@ import { useParams } from 'react-router-dom';
 import Chart from 'react-apexcharts';
 import finnHub from '../apis/finnHub';
 import { useState } from 'react';
+import dayjs from 'dayjs';
+
+const formatData = (data) => {
+    let price = [];
+    // o h l c 순서로
+    for (let i = 0; i < data.o.length; i++) {
+        price.push({
+            x: new Date(data.t[i] * 1000),
+            y: [data.o[i], data.h[i], data.l[i], data.c[i]],
+        });
+    }
+    return price;
+};
 
 export default function StockDetail() {
     const { symbol } = useParams();
+    const [priceData, setPriceData] = useState([]);
     const date = new Date();
 
-    const currentTime = Math.floor(date.getTime() / 1000);
-    let oneDay = currentTime - 2 * 24 * 60 * 60;
-    console.log(currentTime);
-    console.log(oneDay);
-    const [priceData, setPriceData] = useState([]);
-
-    const formatData = (data) => {
-        // o h l c 순서로
-        console.log(data);
-        for (let i = 0; i < data.o.length; i++) {
-            setPriceData((prev) => [
-                ...prev,
-                {
-                    x: data.t[i],
-                    y: [data.o[i], data.h[i], data.l[i], data.c[i]],
-                },
-            ]);
-        }
-    };
-
-    if (priceData) {
-        console.log(priceData);
+    const currentTime = Math.round(date.getTime() / 1000);
+    let oneDay;
+    if (date.getDay() === 6) {
+        // 토요일
+        oneDay = currentTime - 2 * 24 * 60 * 60;
+    } else if (date.getDay() === 0) {
+        // 일요일
+        oneDay = currentTime - 3 * 24 * 60 * 60;
+    } else {
+        oneDay = currentTime - 24 * 60 * 60;
     }
+    const oneWeeks = currentTime - 7 * 24 * 60 * 60;
+    const oneYear = currentTime - 365 * 24 * 60 * 60;
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await finnHub.get('/stock/candle?', {
+                const response = await finnHub.get('/stock/candle', {
                     params: {
-                        symbol: 'IBM',
+                        symbol: symbol,
                         resolution: 30,
                         from: oneDay,
                         to: currentTime,
                     },
                 });
-                formatData(response.data);
+                setPriceData(formatData(response.data));
             } catch (e) {}
         };
         fetchData();
@@ -54,11 +58,15 @@ export default function StockDetail() {
             type: 'candlestick',
         },
         title: {
-            text: 'CandleStick Chart',
             align: 'center',
         },
         xaxis: {
-            type: 'datetime',
+            type: 'category',
+            labels: {
+                formatter: function (value) {
+                    return dayjs(value).format('MMM DD HH:mm');
+                },
+            },
         },
         yaxis: {
             tooltip: {
@@ -79,7 +87,7 @@ export default function StockDetail() {
                 options={chartData}
                 series={chartData.series}
                 type="candlestick"
-                height={350}
+                height={500}
             />
         </div>
     );
